ab0bd589c499aa5b18719529e44a77b27874a392
smilesthesad <vzheng@gmail.com>
2020-08-14 19:02:16 -0400

    basic form for test

---
+++ reacttest/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Box from "./components/Box"
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Box/>
+            </div>
+        )
+    }
+}
+export default App;
